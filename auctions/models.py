9d453c86f6d6d628a16dcedfefa2367decb3b21d
amlupenza <amlupenza@gmail.com>
2023-10-17 11:40:24 +0300

    optional img url and category

--- auctions/models.py
+++ auctions/models.py
@@ -14,9 +14,9 @@ class Category(models.Model):
 
 class Listing(models.Model):
     title = models.CharField(max_length=60)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items")
     description = models.TextField(max_length=1000)
-    image = models.CharField(max_length=1000, blank=True, null=True)
+    image = models.CharField(max_length=1000)
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
     date = models.DateTimeField()
@@ -35,9 +35,6 @@ class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True)
     bid = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     bider = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    
-    def __str__(self):
-        return f"Bid for {self.listing}"
 
 # comments table
 class Comment(models.Model):

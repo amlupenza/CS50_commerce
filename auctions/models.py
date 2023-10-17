f2c2f11b8b1c8c463d1731912ea6aff596ddb8a7
amlupenza <amlupenza@gmail.com>
2023-10-17 10:34:37 +0300

    reverting to another last commit

--- auctions/models.py
+++ auctions/models.py
@@ -16,11 +16,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items")
     description = models.TextField(max_length=1000)
-<<<<<<< HEAD
-    image = models.CharField(max_length=1000)
-=======
     image = models.CharField(max_length=1000, blank=True, null=True)
->>>>>>> parent of dc90984 (last commit)
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
     date = models.DateTimeField()
